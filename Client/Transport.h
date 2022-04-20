6e067b1020e97cdb863df2cb95fc100edda5694d
Ethan Chen <17echen@gmail.com>
2022-04-20 16:09:58 -0400

    updates

--- Client/Transport.h
+++ Client/Transport.h
@@ -1,13 +1,15 @@
 #ifndef H_TRANSPORT
 #define H_TRANSPORT
+#include <string>
 
 class Transport
 {
 private:
 
 public:
-    Transport(char c);
+    int send(short address, short port, std::string message);
 
+    int receive();
 };
 
 #endif //H_TRANSPORT
