025103a5dfc55dc7c3a461dc2627ecf8a6ec8050
shaun-gentilin <shaun.gentilin@hotmail.com>
2022-04-21 14:08:28 -0400

    server mock transport

---
+++ Server/mockTransport.cpp
@@ -0,0 +1,12 @@
+#include "Datalink.h"
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+int main() {
+  Datalink link = new Datalink();
+  char* packet = link.recv
+  char* ret;
+  memcpy(ret, packet, 64);
+  return ret;
+}
