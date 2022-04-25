5d5d3359065721873bbfcd0dddf3851cd5e95d9b
shaun-gentilin <84873304+shaun-gentilin@users.noreply.github.com>
2022-04-25 09:52:54 -0400

    Add files via upload

---
+++ mockTransport.cpp
@@ -0,0 +1,13 @@
+#include "Datalink.h"
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+int main() {
+  Datalink* link = new Datalink();
+  char* packet = link->recv();
+  char* ret;
+  memcpy(ret, packet, 64);
+  cout << ret << endl;
+  return 0;
+}
