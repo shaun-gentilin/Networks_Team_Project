732d97791b1e59bcc332ea08651895527c87b728
Ethan Chen <17echen@gmail.com>
2022-04-25 15:19:04 -0400

    transport changes

--- Client/Transport.cpp
+++ Client/Transport.cpp
@@ -18,21 +18,8 @@ int Transport::send(short address, short port, std::string message)
     c[65] = address & 0xffff;
     c[64] = (address & 0xffff) >> 8;
 
-    Datalink d;
-    d.send(c, 5);
-
-    /*
-    std::string totalMessage = message + std::to_string(address) + std::to_string(port);
-    int totalMessageSize = totalMessage.size();
-    char* c = new char[totalMessageSize];
-    memcpy(c, totalMessage.c_str(), totalMessageSize);
-    
-    //Send to datalink
-    
-
-    delete c;
-    return 1;
-    */
+    //Datalink d;
+    //d.send(c, 5);
 }
 
 int Transport::receive()
