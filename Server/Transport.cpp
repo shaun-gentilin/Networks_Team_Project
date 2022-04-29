b1d48940d7ba4ea16c208ce8ac9a7462c124fefb
Ethan Chen <17echen@gmail.com>
2022-04-29 17:05:32 -0400

    Update Transport.cpp

--- Server/Transport.cpp
+++ Server/Transport.cpp
@@ -18,9 +18,19 @@ char* Transport::receive()
 
     short port = (m[67] & 0xfff) | ((m[66] & 0xffff) << 8);
     std::cout << "PORT: " << port << std::endl;
+    if (port != 18)
+    {
+        std::cout << "Error invalid port number" << std::endl;
+        //exit();
+    }
 
     short address = (m[65] & 0xfff) | ((m[64] & 0xffff) << 8);
     std::cout << "ADDRESS: " << address << std::endl;
+    if (address != 27)
+    {
+        std::cout << "Error invalid address number" << std::endl;
+        //exit();
+    }
     
-    return d.recv();
+    return m;
 }
